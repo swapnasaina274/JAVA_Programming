917adda336cd6d8b4243d137f9168008c1e64678
swapnasaina274 <sainaswapna274@gmail.com>
2026-08-15 14:55:53 +0530

    Add files via upload

---
+++ Practice/Set-1/KmToMiles.java
@@ -0,0 +1,9 @@
+class KmToMiles
+{
+    public static void main(String[] args)
+    {
+       double km = 1;
+       double miles = km * 0.621371;
+       System.out.println(km + " kilometer is "+ miles+" miles");;
+    }
+}
