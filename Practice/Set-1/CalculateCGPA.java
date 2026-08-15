4eed588752a3f65579a95610176f416424aa6807
swapnasaina274 <sainaswapna274@gmail.com>
2026-08-15 14:51:47 +0530

    Add files via upload

---
+++ Practice/Set-1/CalculateCGPA.java
@@ -0,0 +1,11 @@
+class CalculateCGPA
+{
+    public  static void main(String[] args)
+    {
+        float subject1 = 86;
+        float subject2 = 95;
+        float subject3 = 82;
+        float cgpa = (subject1+subject2+subject3)/30;
+        System.out.println(cgpa);
+    }
+}
