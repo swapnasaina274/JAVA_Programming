3a1313c54bb49a47d7a17485ed01423a05eb8779
swapnasaina274 <sainaswapna274@gmail.com>
2026-08-22 13:21:57 +0530

    Add files via upload

---
+++ Basics/VariableDeclaration.java
@@ -0,0 +1,13 @@
+public class VariableDeclaration {
+    public static void main(String[] args) {
+        double testScore = 85.5;
+        boolean passed = true;
+        char gradeLevel = 'A';
+        int studentId = 12345;
+
+        System.out.println("Test Score: " + testScore);
+        System.out.println("Pass Status: " + passed);
+        System.out.println("Grade Level: " + gradeLevel);
+        System.out.println("Student ID: " + studentId);
+    }
+}
