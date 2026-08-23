25e2c7fa86792636f7e8c828058ec7bfcff04a05
swapnasaina274 <sainaswapna274@gmail.com>
2026-08-23 09:51:39 +0530

    Add files via upload

---
+++ Basics/TypeCast.java
@@ -0,0 +1,11 @@
+class TypeCast
+{
+    public static void main(String[] args)
+    {
+        double d = 10.5;
+        int i = (int) d; // Explicit type casting from double to int
+
+        System.out.println("Double value: " + d);
+        System.out.println("Integer value after casting: " + i);
+    }
+}
