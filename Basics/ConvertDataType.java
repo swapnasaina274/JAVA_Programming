1ff0f84a1141a4c9533d87a396b0ce1cd4229963
swapnasaina274 <sainaswapna274@gmail.com>
2026-08-23 09:57:59 +0530

    Add files via upload

---
+++ Basics/ConvertDataType.java
@@ -0,0 +1,11 @@
+class ConvertDataType
+{
+    public static void main(String[] args)
+    {
+        int value1=50;
+        int value2=20;
+
+        double result=(double)value1/value2;
+        System.out.println("Result:" +result);
+    }
+}
