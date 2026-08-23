185d0f0956fcc6e84789c6f5496c07ab4b21db93
swapnasaina274 <sainaswapna274@gmail.com>
2026-08-23 10:04:17 +0530

    Add files via upload

---
+++ Basics/Perimeter_Area_Rectangle.java
@@ -0,0 +1,14 @@
+class Perimeter_Area_Rectangle
+{
+    public static void main(String[] args)
+    {
+        float length = 5.6f;
+        float width = 2.3f;
+
+        float perimeter = 2 * (length + width);
+        float area = length * width;
+
+        System.out.println("Perimeter of the rectangle: " + perimeter);
+        System.out.println("Area of the rectangle: " + area);
+    }
+}
