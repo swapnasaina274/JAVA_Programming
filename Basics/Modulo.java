1a01f12abc238bbb2fd563cff0b5cf2aaf19a98a
swapnasaina274 <sainaswapna274@gmail.com>
2026-08-24 20:07:36 +0530

    Add files via upload

---
+++ Basics/Modulo.java
@@ -0,0 +1,15 @@
+class Modulo
+{
+    public static void main(String[] args)
+    {
+        int number=67;
+        if(number%2==0)
+        {
+            System.out.println("Even Number");
+        }
+        else
+        {
+            System.out.println("Odd Number");
+        }
+    }
+}
