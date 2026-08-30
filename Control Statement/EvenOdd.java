11c8af2b94d90642a2eac1e1ccb156f64c7eec02
swapnasaina274 <sainaswapna274@gmail.com>
2026-08-30 20:11:37 +0530

    Add files via upload

---
+++ Control Statement/EvenOdd.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class EvenOdd
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num = sc.nextInt();
+
+        if(num % 2 == 0)
+        {
+            System.out.println(num + " is an even number.");
+        }
+        else
+        {
+            System.out.println(num + " is an odd number.");
+        }
+    }
+}
