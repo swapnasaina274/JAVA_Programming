785d6d1ad77a65545c33d7dfe4e7d371ae715022
swapnasaina274 <sainaswapna274@gmail.com>
2026-08-30 20:09:17 +0530

    Create PositiveNegativeZero.java

---
+++ Control Statement/PositiveNegativeZero.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class PositiveNegativeZero
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num = sc.nextInt();
+
+        if(num > 0)
+        {
+            System.out.println(num + " is a positive number.");
+        }
+        else if(num < 0)
+        {
+            System.out.println(num + " is a negative number.");
+        }
+        else
+        {
+            System.out.println("The number is zero.");
+        }
+    }
+}
