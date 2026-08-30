10e710a32cee2883bff58e6d0e499f85fbc60aca
swapnasaina274 <sainaswapna274@gmail.com>
2026-08-30 20:19:36 +0530

    Add files via upload

---
+++ Control Statement/LargestOfTwo.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class LargestOfTwo
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the first number:");
+        int num1 = sc.nextInt();
+
+        System.out.println("Enter the second number:");
+        int num2 = sc.nextInt();
+
+        if(num1 > num2)
+        {
+            System.out.println(num1+"is the largest number.");
+        }
+        else if(num2 > num1)
+        {
+            System.out.println(num2+"is the largest number.");
+        }
+        else
+        {
+            System.out.println("Both numbers are equal.");
+        }
+
+    }
+}
