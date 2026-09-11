f1a64604e11e32757995c0dbd6f82626fc4381b3
swapnasaina274 <sainaswapna274@gmail.com>
2026-09-11 23:36:06 +0530

    Create PrintArrayElements.java

---
+++ Arrays/PrintArrayElements.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class PrintArrayElements
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array:");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+
+        System.out.println("Enter the elements of the array:");
+        for(int i=0; i<size; i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+
+        
+        System.out.println("Print the elements of the array:");
+        for(int i=0; i<size; i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+
+
+    }
+}
