9d9c759b7f159f4f4604684ab87c916dabff2c14
swapnasaina274 <sainaswapna274@gmail.com>
2026-09-14 20:40:05 +0530

    Add files via upload

---
+++ Arrays/CopyArray.java
@@ -0,0 +1,16 @@
+public class CopyArray {
+    public static void main(String[] args) {
+        int[] arr1 = {10, 20, 30, 40, 50};
+        int[] arr2 = new int[arr1.length];
+
+        for (int i = 0; i < arr1.length; i++) {
+            arr2[i] = arr1[i];
+        }
+
+        System.out.println("Elements of copied array:");
+
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i] + " ");
+        }
+    }
+}
